3852e709765e49f91ca5e2fe52d82d0c9fe636e5
Dylan Medina <dlondonmedina@gmail.com>
2021-01-29 14:45:32 -0800

    main updated

--- backend/go.mod
+++ backend/go.mod
@@ -1,3 +1,5 @@
 module chatapp
 
 go 1.15
+
+require github.com/gorilla/websocket v1.4.2
---
+++ backend/main.go
@@ -0,0 +1,38 @@
+package main
+
+import (
+	"chatapp/pkg/websocket"
+	"fmt"
+	"net/http"
+)
+
+func serveWs(pool *websocket.Pool, w http.ResponseWriter, r *http.Request) {
+	fmt.Println("Websocket Endpoint Hit")
+	conn, err := websocket.Upgrade(w, r)
+	if err != nil {
+		fmt.Fprintf(w, "%+v\n", err)
+	}
+
+	client := &websocket.Client{
+		Conn: conn,
+		Pool: pool,
+	}
+
+	pool.Register <- client
+	client.Read()
+}
+
+func setupRoutes() {
+	pool := websocket.NewPool())
+	go pool.Start()
+
+	http.HandleFunc("/ws", func(w http.ResponseWriter, r *http.Request) {
+		serveWs(pool, w, r)
+	})
+}
+
+func main() {
+	fmt.Println("Distributed Chat App v0.0.1")
+	setupRoutes()
+	http.ListenAndServe(":5000", nil)
+}
